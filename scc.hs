8f1921c5204e1d37ac45b6d1638a20e18ad2d7bc
Arda Cinar <kuzux92@gmail.com>
2014-08-26 22:38:21 +0300

    solved scc

---
+++ scc.hs
@@ -0,0 +1,20 @@
+import Control.Monad
+import Data.Functor
+import Data.Graph
+
+mkGraph :: Int -> [(Int, Int)] -> Graph
+mkGraph n es = fst3 . graphFromEdges $ [(i, i, go i)| i <- [1..n]]
+    where go i = map snd . filter ((==i) . fst) $ es
+          fst3 (a,_,_) = a
+
+
+readGraph :: IO (Int, [(Int, Int)])
+readGraph = do (n:m:_) <- map read . words <$> getLine
+               edges <- replicateM m $ toTuple . map read . words <$> getLine
+               return (n, edges)
+    where toTuple (a:b:_) = (a,b)
+
+main :: IO ()
+main = print =<< length . scc . (\(n,e) -> mkGraph n e) <$> readGraph
+          
+          
